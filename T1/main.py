eaae94473b169b5f637e9cc91fbcb82d950e7ea0
AndreDRI <andrewdrisostes@gmail.com>
2026-09-14 19:33:22 -0300

    (feat): input reading

--- T1/main.py
+++ T1/main.py
@@ -1,3 +1,14 @@
-n = int(input())
-print(n)
-print(n + 1)
+
+
+def read_input():
+    vec = []
+    N = int(input())
+    for i in range(N):
+        vec.append([int(x) for x in input().split()])
+    return vec
+
+def main():
+    points = read_input()
+    print(points)
+
+main()
