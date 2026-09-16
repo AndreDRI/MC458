28f0ea1333487a1c93faaeda24622ea513fae32a
AndreDRI <andrewdrisostes@gmail.com>
2026-09-16 20:02:32 -0300

    (feat): final solution

--- T1/main.py
+++ T1/main.py
@@ -57,20 +57,33 @@ def read_input():
     P = []
     N = int(input())
     for i in range(N):
-        P.append([int(x) for x in input().split()])
+        line = [int(x) for x in input().split()]
+        line.append(i)
+        P.append(line)
     return P
 
 def main():
     points = read_input()
     sorted_x = merge_sort(0, len(points), points, "X")
     sorted_y = merge_sort(0, len(points), points, "Y")
-    print(closestPair(sorted_x, sorted_y))
+    min_dist, solution = closestPair(sorted_x, sorted_y)
+
+    for planes in sorted(solution):
+        print(planes)
 
 def distance(point_A, point_B):
     return sqrt(((point_A[0] - point_B[0]) ** 2) + ((point_A[1] - point_B[1]) ** 2))
 
 def sqrt(expression):
     return expression ** (1 / 2)
+
+def get_plane_index(point_A, point_B):
+    plane_one = point_A[2]
+    plane_two = point_B[2]
+    if plane_one < plane_two:
+        return f"({plane_one},{plane_two})"
+    else:
+        return f"({plane_two},{plane_one})"
 
 def slice_y(vec_y, mid_x, left_mid_count):
     left_y = []
@@ -89,11 +102,28 @@ def slice_y(vec_y, mid_x, left_mid_count):
 def closestPair(vec_x, vec_y):
     # N points in 2D Space
     N = len(vec_x)
+    FLOAT_DIFF = 1e-9
     # Base Cases
     if (N == 2):
-        return distance(vec_x[0], vec_x[1])
+        return distance(vec_x[0], vec_x[1]), [get_plane_index(vec_x[0], vec_x[1])]
     if (N == 3):
-        return min(distance(vec_x[0], vec_x[1]), distance(vec_x[0], vec_x[2]), distance(vec_x[1], vec_x[2]))
+        plane_zero_one = distance(vec_x[0], vec_x[1]) 
+        plane_zero_two = distance(vec_x[0], vec_x[2])
+        plane_one_two = distance(vec_x[1], vec_x[2])
+        min_dist = min(plane_zero_one, plane_zero_two, plane_one_two)
+
+        pairs = []
+        if abs(plane_zero_one - min_dist) < FLOAT_DIFF:
+            pairs.append(get_plane_index(vec_x[0], vec_x[1]))
+        if abs(plane_zero_two - min_dist) < FLOAT_DIFF:
+            pair = get_plane_index(vec_x[0], vec_x[2])
+            if pair not in pairs:
+                pairs.append(pair)
+        if abs(plane_one_two - min_dist) < FLOAT_DIFF:
+            pair = get_plane_index(vec_x[1], vec_x[2])
+            if pair not in pairs:
+                pairs.append(pair)
+        return min_dist, pairs   
     # Divide Step, dividing 2D plane on smaller planes, and recursively calculating
     # Left and Right Halves
     left_plane, right_plane = slice(0, len(vec_x), vec_x)
@@ -111,11 +141,21 @@ def closestPair(vec_x, vec_y):
     left_plane_y, right_plane_y = slice_y(vec_y, mid_x, left_mid_count)
 
     # Recursion
-    left_plane_distance = closestPair(left_plane, vec_y)
-    right_plane_distance = closestPair(right_plane, vec_y)
-    epsilon = min(left_plane_distance, right_plane_distance)
+    left_plane_distance, pairs_left = closestPair(left_plane, left_plane_y)
+    right_plane_distance, pairs_right = closestPair(right_plane, right_plane_y)
 
     # Combine Step, be S which are the points in Y whose x-coor are in range of (x - d, x + d)
+    # Combining left and right pairs
+    if abs(left_plane_distance - right_plane_distance) < FLOAT_DIFF:
+        epsilon = left_plane_distance
+        best_pairs = pairs_left + pairs_right
+    elif left_plane_distance < right_plane_distance:
+        epsilon = left_plane_distance
+        best_pairs = pairs_left
+    else:
+        epsilon = right_plane_distance
+        best_pairs = pairs_right
+
     # Building strip
     strip = []
     for i in range(len(vec_y)):
@@ -125,11 +165,19 @@ def closestPair(vec_x, vec_y):
     # Checking strip for the seven neighboring points
     for i in range(len(strip)):
         for j in range(i + 1, min(i + 8, len(strip))):
-            if ((strip[j][1] - strip[i][1]) >= epsilon):
+            if ((strip[j][1] - strip[i][1]) >= epsilon + FLOAT_DIFF):
                 break
-            epsilon = min(epsilon, distance(strip[i], strip[j]))
+            min_d = distance(strip[i], strip[j])
 
-    return epsilon
+            if min_d < epsilon - FLOAT_DIFF:
+                epsilon = min_d
+                best_pairs = [get_plane_index(strip[i], strip[j])]
+            elif abs(min_d - epsilon) < FLOAT_DIFF:
+                pair = get_plane_index(strip[i], strip[j])
+                if pair not in best_pairs:
+                    best_pairs.append(pair)
+        
+    return epsilon, best_pairs
 
 main()
 
